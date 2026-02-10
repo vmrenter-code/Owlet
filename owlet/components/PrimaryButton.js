c71b940bf6eecb0c1988621b0bcf4fae1cc45de3
Catherine De Los Santos <cathered@uci.edu>
2026-02-09 23:11:48 -0800

    renamed button component to primarybutton to avoid native name conflicts

--- owlet/components/PrimaryButton.js
+++ owlet/components/PrimaryButton.js
@@ -1,8 +1,8 @@
 import {View, Text} from 'react-native';
-function Button({children}) {
+function PrimaryButton({children}) {
     return <View>
         <Text>{children}</Text>
     </View>
 }
 
-export default Button;
+export default PrimaryButton;
