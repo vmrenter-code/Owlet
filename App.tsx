aabd072d5dcce30139bf15752e56b413e81169c7
Eliran Avigani <eavi0001@gmail.com>
2026-02-10 11:27:10 -0800

    organized the project structure. Updated import statements accordingly.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { View, Text, Button } from 'react-native';
 
-import Launch from './screens/Launch';
+import Launch from './owlet/screens/Launch';
 
 export default function App() {
   return <Launch />;
